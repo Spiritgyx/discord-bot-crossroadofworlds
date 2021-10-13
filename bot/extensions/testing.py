98c536bd369aa87dafbbbe93bfc219f4b32c4491
Spiritgyx <megysmev@gmail.com>
2021-10-13 16:12:38 +0300

    Added command "me".

--- bot/extensions/testing.py
+++ bot/extensions/testing.py
@@ -16,6 +16,7 @@ class Testing(commands.Cog):
     @commands.command()
     async def get_current_members(self, ctx: commands.Context):
         sql_members = self.sql.get_members(ctx.guild.id)
+        self.logger.debug(str(sql_members))
         await ctx.send('\n'.join(map(str, sql_members)))
 
     @commands.command()
@@ -23,6 +24,38 @@ class Testing(commands.Cog):
         e = discord.Embed(title="BAM", description="Pif puf paf", colour=random.randint(0x00, 0xffffff))
         await ctx.send(embed=e)
 
+    @commands.command()
+    async def me(self, ctx: commands.Context, member: discord.Member=None):
+        if member is None:
+            target = ctx.message.author
+        else:
+            target = member
+
+        e = discord.Embed(title=f"{target.name}#{target.discriminator}",
+                          description=f"{target.display_name or target.name}",
+                          colour=random.randint(0x00, 0xffffff),
+                          url='https://discord.gg/qMMskt4Z')
+        e.add_field(name="Mention", value=f"{target.mention}", inline=False)
+        e.add_field(name="Joined", value=f"{target.joined_at}", inline=True)
+        e.add_field(name="GuildID", value=f"{target.guild.id}", inline=True)
+        e.add_field(name="MemberID", value=f"{target.id}", inline=True)
+        e.add_field(name="voice?", value=f"{target.voice}", inline=False)
+        e.add_field(name="color?", value=f"{target.color}")
+        e.add_field(name="colour?", value=f"{target.colour}")
+        e.add_field(name="desktop_status?", value=f"{target.desktop_status}")
+        e.add_field(name="guild_permissions?", value=f"{target.guild_permissions}")
+        e.add_field(name="activity?", value=f"{target.activity}")
+        e.add_field(name="roles?", value=f"{target.roles}")
+        e.add_field(name="top_role?", value=f"{target.top_role}")
+        e.add_field(name="avatar_url?", value=f"{target.avatar_url}")
+        e.set_footer(text=f"Down information", icon_url=target.avatar_url)
+        e.set_image(url=target.avatar_url)
+        e.set_author(name=f"{target.name}#{target.discriminator}",
+                     url="https://discord.gg/qMMskt4Z",
+                     icon_url=target.avatar_url)
+        # member.avatar_url
+        # target.roles
+        await ctx.send(embed=e)
 
 def setup(client: bot.mybot.Bot):
     client.add_cog(Testing(client))
